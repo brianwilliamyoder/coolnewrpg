890f5ddccde376da3870cf54ce26f35279f31927
Brian Yoder <brianyoder@gmail.com>
2022-12-18 13:35:43 -0800

    1st test PASS
    Co-authored by: Tanya Prado <tanyaprado@gmail.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -3,6 +3,8 @@ import { Character } from './../src/character.js';
 describe('Character', () => {
   test('should create a character object with properties', () => {
     const character = new Character();
-    expect(character).toEqual();
+    expect(character.position).toEqual(undefined);
+    expect(character.stamina).toEqual(undefined);
+    expect(character.temperament).toEqual(undefined);
   })
 });
