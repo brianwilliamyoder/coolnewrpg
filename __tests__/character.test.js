2033dbd64419977efc938d8beb7ed239e1fb66d1
Brian Yoder <brianyoder@gmail.com>
2022-12-18 12:03:10 -0800

    1st Test: good fail Co-authored by: Tanya Prado <tanyaprado@gmail.com>

---
+++ __tests__/character.test.js
@@ -0,0 +1,8 @@
+import { Character } from './../src/character.js';
+
+describe('Character', () => {
+  test('should create a character object with properties', () => {
+    const character = new Character();
+    expect(character).toEqual();
+  })
+});
