5e87485d872ae023c3290ef1672bae1a0cb716e4
Brian Yoder <brianyoder@gmail.com>
2022-12-18 14:19:02 -0800

    2nd test failed as expected Co-authored by: Tanya Prado <tanyaprado@gmail.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -6,5 +6,14 @@ describe('Character', () => {
     expect(character.position).toEqual(undefined);
     expect(character.stamina).toEqual(undefined);
     expect(character.temperament).toEqual(undefined);
-  })
+    expect(character.inventory).toEqual(undefined);
+  });
+
+  test('should update stamina and temperament values', () => {
+    const character = new Character();
+    expect(character.stamina).toEqual(1);
+    expect(character.temperament).toEqual(1);
+  });
+
+
 });
--- src/character.js
+++ src/character.js
@@ -1,7 +1,13 @@
 export class Character {
-  constructor (position, stamina, temperament) {
+  constructor (position, stamina, temperament, inventory) {
     this.position = position;
-    this.stamina =  stamina
-    this.temperament = temperament;
+    this.stamina = stamina;
+    this.temperament = temperament
+    this.inventory = inventory;;
+  }
+
+  gainExperience() {
+    this.stamina += 1;
+    this.temperament += 1;
   }
  }
