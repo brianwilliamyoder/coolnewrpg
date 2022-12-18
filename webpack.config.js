9a1ce9effcaf2a3ecb038ed34371b0854698f794
Brian Yoder <brianyoder@gmail.com>
2022-12-18 12:54:40 -0800

    update pack-lock.json automatic

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // new line
+const ESLintWebpackPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,8 +9,15 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devServer: {
+    contentBase: './dist'
+  },
+  devtool: 'eval-source-map',
   plugins: [
-    new CleanWebpackPlugin(), // new line
+    new ESLintWebpackPlugin(), 
+    new CleanWebpackPlugin({
+      verbose: true
+    }),
     new HtmlWebpackPlugin({
       title: 'Shape Tracker',
       template: './src/index.html',
