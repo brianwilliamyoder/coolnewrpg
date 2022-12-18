63376a852a173521a04a9870f46bfe6c88bb34f1
Brian Yoder <brianyoder@gmail.com>
2022-12-18 10:53:34 -0800

    add CleanWebpackPlugin Co-authored by: Tanya Prado <tanyaprado@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
-
 const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // new line
 
 module.exports = {
   entry: './src/index.js',
@@ -9,8 +8,8 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-
   plugins: [
+    new CleanWebpackPlugin(), // new line
     new HtmlWebpackPlugin({
       title: 'Shape Tracker',
       template: './src/index.html',
